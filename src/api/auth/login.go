31030a4615ae4245c69d2effa09debee87ff4fcc
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-01 17:41:11 +0300

    login in site add

--- src/api/auth/login.go
+++ src/api/auth/login.go
@@ -2,8 +2,11 @@ package auth
 
 import (
 	"encoding/json"
-	"net/http"
+	"os"
 
+	"github.com/Cthulhu-tech/store/src/utils/database"
+	"github.com/Cthulhu-tech/store/src/utils/jwt"
+	"github.com/Cthulhu-tech/store/src/utils/message"
 	"github.com/labstack/echo/v4"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -11,16 +14,80 @@ import (
 
 func Login(c echo.Context) error {
 
-	json_map := make(map[string]interface{})
+	refreshDuration := 10080
 
-	err := json.NewDecoder(c.Request().Body).Decode(&json_map)
+	user := UserLogin{}
+
+	err := json.NewDecoder(c.Request().Body).Decode(&user)
 
 	if err != nil {
 
-		return err
+		return message.JSON(c, 403, "Need all parameters")
 
 	}
 
-	return c.String(http.StatusOK, "login")
+	if user.Password == "" || user.Login == "" || !isEmailValid(user.Mail) {
+
+		return message.JSON(c, 403, "Need all parameters")
+
+	}
+
+	db := database.GetDB()
+
+	rows, err := db.Query("SELECT * FROM users WHERE email = ? AND login = ? AND confirm = 1", user.Mail, user.Login)
+
+	if err != nil {
+
+		return message.JSON(c, 403, "User not found")
+
+	}
+
+	userInfo := UserAllData{}
+
+	for rows.Next() {
+
+		userCheck := UserAllData{}
+
+		if err := rows.Scan(&userCheck.Id, &userCheck.Login, &userCheck.Email, &userCheck.Password, &userCheck.Confirm); err != nil {
+
+			return message.JSON(c, 403, "User not found")
+
+		}
+
+		userInfo.Id = userCheck.Id
+		userInfo.Login = userCheck.Login
+		userInfo.Email = userCheck.Email
+		userInfo.Password = userCheck.Password
+		userInfo.Confirm = userCheck.Confirm
+
+	}
+
+	refresh, err := jwt.CreateToken(userInfo.Login, userInfo.Id, refreshDuration, os.Getenv("JWT_SECRET_REFRESH"))
+
+	if err != nil {
+
+		return message.JSON(c, 500, "Server error")
+
+	}
+
+	access, err := jwt.CreateToken(userInfo.Login, userInfo.Id, 30, os.Getenv("JWT_SECRET_BEARER"))
+
+	if err != nil {
+
+		return message.JSON(c, 500, "Server error")
+
+	}
+
+	_, err = db.Query("INSERT INTO token (user_id, jwt) VALUES (?, ?)", userInfo.Id, refresh)
+
+	if err != nil {
+
+		return message.JSON(c, 500, "Server error")
+
+	}
+
+	jwt.SetCookie(c, refreshDuration, refresh)
+
+	return c.JSON(201, &MessageToken{Message: "You login", Token: access})
 
 }
