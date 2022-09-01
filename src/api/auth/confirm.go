f1f1ece9c91893855ae92aa8de998f099e5041cd
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-01 17:41:43 +0300

    confirm logic end

--- src/api/auth/confirm.go
+++ src/api/auth/confirm.go
@@ -1,23 +1,35 @@
 package auth
 
 import (
+	"encoding/json"
+	"log"
+
 	"github.com/Cthulhu-tech/store/src/utils/database"
+	"github.com/Cthulhu-tech/store/src/utils/message"
 	"github.com/labstack/echo/v4"
 )
 
 func Confirm(c echo.Context) error {
 
-	code := c.QueryParam("code")
+	var data = &UserConfirm{}
 
-	if len(code) == 0 {
+	err := json.NewDecoder(c.Request().Body).Decode(&data)
 
-		return invalidQueryParam(c)
+	if err != nil {
+
+		return err
+
+	}
+
+	if data.Code == "" || data.Secret == 0 {
+
+		return message.JSON(c, 403, "Invalid body parameters")
 
 	}
 
 	db := database.GetDB()
 
-	_, err := db.Query("SELECT ", code)
+	rows, err := db.Query("SELECT `sp_confirm`(?, ?) AS `sp_confirm`", data.Code, data.Secret)
 
 	if err != nil {
 
@@ -25,6 +37,28 @@ func Confirm(c echo.Context) error {
 
 	}
 
-	return nil
+	value := &ConfirmFunc{}
+
+	for rows.Next() {
+
+		var confirm ConfirmFunc
+
+		if err := rows.Scan(&confirm.Value); err != nil {
+
+			log.Println(err.Error())
+
+		}
+
+		value.Value = confirm.Value
+
+	}
+
+	if value.Value == 1 {
+
+		return message.JSON(c, 201, "User confirm")
+
+	}
+
+	return message.JSON(c, 403, "Confirm error")
 
 }
